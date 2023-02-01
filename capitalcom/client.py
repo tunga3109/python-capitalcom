4f2e324f2027052a66e73ad80f6bce128b51957d
Tung <tunga3109@gmail.com>
2023-02-01 14:50:35 +0200

    updates

--- capitalcom/client.py
+++ capitalcom/client.py
@@ -3,10 +3,12 @@ from enum import Enum
 
 import requests
 
+from config import *
+
 class CapitalComConstants():
     HEADER_API_KEY_NAME = 'X-CAP-API-KEY'
     API_VERSION = 'v1'
-    BASE_URL = 'https://api-capital.backend-capital.com/api/{}/'.format(
+    BASE_URL = 'https://demo-api-capital.backend-capital.com/api/{}/'.format(
         API_VERSION
     )
     BASE_DEMO_URL = 'https://demo-api-capital.backend-capital.com/api/{}/'.format(
@@ -55,6 +57,7 @@ class TakeProfitType(Enum):
     DISTANCE = 'profitDistance'
     AMOUNT = 'profitAmount'
 
+key = 'kdkjekjdsjkdkjd'
 
 
 class Client():
@@ -64,15 +67,82 @@ class Client():
     API documantation here: https://capital.com/api-development-guide
     """
 
-    def __init__(self, api_key):
+    def __init__(self, login, password, api_key):
+        self.login = login
+        self.password = password
         self.api_key = api_key
+
 
     
     def _get_header(self, **kwargs):
         return {
-            **kwargs,
-            CapitalComConstants.HEADER_API_KEY_NAME: self.api_key
+            **kwargs
         }
+
+    def _get(self, url, **kwargs):
+        return requests.get(url, **kwargs)
+
+    def _post(self, url, **kwargs):
+        return requests.post(url, **kwargs)
+
+    def _delete(self, url, **kwargs):
+        return requests.delete(url, **kwargs)
+
+    def start_session(self):
+        r = self._post(
+            CapitalComConstants.SESSION_ENDPOINT, 
+            json={'identifier': login, 'password': password},
+            headers={CapitalComConstants.HEADER_API_KEY_NAME: self.api_key}
+            )
+        return r.json()
+    
+    def _get_cst(self) -> str:
+        r = self._post(
+            CapitalComConstants.SESSION_ENDPOINT, 
+            json={'identifier': login, 'password': password},
+            headers={CapitalComConstants.HEADER_API_KEY_NAME: self.api_key}
+            )
+
+        return r.headers['CST']
+
+    def _get_x_security_token(self) -> str:
+        r = self._post(
+            CapitalComConstants.SESSION_ENDPOINT, 
+            json={'identifier': login, 'password': password},
+            headers={CapitalComConstants.HEADER_API_KEY_NAME: self.api_key}
+            )
+
+        return r.headers['X-SECURITY-TOKEN']
+
+    
+    def accounts_information(self, cst, x_token): #!!!!!!!!!!!!
+        r = self._get(
+            CapitalComConstants.SESSION_ENDPOINT,
+            headers={
+                'CST': cst,
+                'X-SECURITY-TOKEN': x_token
+            }
+        )
+
+        return r.json()
+
+
+
+
+
+# cl = Client(login, password, API_KEY)
+
+# c = cl._get_cst()
+# x = cl._get_x_security_token()
+
+# print(cl.accounts_information(cst='j4zKbE4dncy7SQH0pTg4v1NM', x_token='dBntDf8bsCiS76hDNElLmzpL5xh12nL'))
+
+
+
+
+
+
+
 
 
 
---
+++ capitalcom/config.py
@@ -0,0 +1,6 @@
+token = '5556888362:AAEeI3EEspZM65LRV74nQdzXweDBLiT9GJA'
+
+
+API_KEY = 'QFSmCKkrSYlz6Igx' #expired 24 hours
+login = 'tunga3109@gmail.com'
+password = 'Xuxin_10031999'
