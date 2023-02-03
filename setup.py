d620cf55927645b521630bb16865d3c1187f2ec4
Tung <tunga3109@gmail.com>
2023-02-03 06:27:08 +0200

    updated version to 0.2.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='python-capitalcom',
-    version='0.2.5',
+    version='0.2.6',
     packages=['capitalcom'],
     description='Capital.com REST API python implementation',
     long_description=long_description,
