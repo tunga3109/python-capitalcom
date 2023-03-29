8e299bf88ac10e00878e0bca9fbcf303d6f704c1
Tung <tunga3109@gmail.com>
2023-03-29 21:52:17 +0300

    test

--- capitalcom/tests.py
+++ capitalcom/tests.py
@@ -2,4 +2,13 @@ from client_demo import *
 from config import *
 
 cl = Client(login, password, API_KEY)
-print(cl.update_the_order(dealId='000940dd-0055-311e-0000-000081864126', level=28299))
+
+lev_1 = int(input('first leverage: '))
+lev_2 = int(input('second leverage: '))
+print(cl.account_preferences())
+print(cl.update_account_preferences({"CRYPTOCURRENCIES": lev_1}))
+
+print(cl.place_the_position(direction=DirectionType.BUY, epic='BTCUSD', size=1))
+
+print(cl.update_account_preferences({"CRYPTOCURRENCIES": lev_2}))
+print(cl.account_preferences())
