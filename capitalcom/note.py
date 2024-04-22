93bdb40f33cc6356204b4d0b6cbb6a1bd2cef4a4
Tung <tunga3109@gmail.com>
2024-04-23 00:28:33 +0300

    added

--- capitalcom/note.py
+++ capitalcom/note.py
@@ -1,4 +1,14 @@
 from config import login, password, API_KEY
+import time
+
+def measure_time(func):
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+    return wrapper
+
 
 from client_demo_beta_version import *
 
@@ -8,5 +18,10 @@ session = AccountDetails(
     API_KEY
 )
 
+@measure_time
+def margin():
+    print(UserSettings.margin_calculation(1,1,1))
+    
+margin()
 
 a = ''
